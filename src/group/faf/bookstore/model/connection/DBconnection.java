b430b49262f33591f936ba444364938d1a20667f
long luu <hailongluu@gmail.com>
2019-05-09 01:29:55 +0700

    update

--- src/group/faf/bookstore/model/connection/DBconnection.java
+++ src/group/faf/bookstore/model/connection/DBconnection.java
@@ -17,7 +17,7 @@ public class DBconnection {
         if(connect == null){
             hasDriver();
             this.connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/oebc?" +
-                    "user=root&password=");
+                    "user=root&password=111111");
         }
         return connect;
     }
